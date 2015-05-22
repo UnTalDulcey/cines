c08a4370fb201fcbc90457b013e876cbe27a98d9
CristianDulcey <christian.dulcey@gmail.com>
2015-05-21 21:34:50 -0500

    correccion db

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 var Sequelize = require("sequelize")
-var sequelize = new Sequelize('cines', 'cine', 'cines.2015', {
-  host: "cines-app-db.database.windows.net",
-  port: 1433,
-  dialect: 'mssql',
+var sequelize = new Sequelize('cines', 'root', 'mdulcey93', {
+  host: "107.170.248.123",
+  port: 3306,
+  dialect: 'mysql',
 })
-sequelize.authenticate().then(function(err) {
-    console.log('Connection has been established successfully.');
-  }, function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
 module.exports = {
   sequelize: sequelize,
 }
